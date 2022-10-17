79a8bb5fea8fb1ed9d20c3d20c2f7b9ec0df67bc
stanley omeje <stanley.omeje>
2022-10-17 07:15:54 +0100

    created knexfile

---
+++ src/database/knexfile.ts
@@ -0,0 +1,54 @@
+import {Knex} from "knex"
+import path from 'node:path'
+// Update with your config settings.
+interface IKnexConfig {
+  [key: string]: Knex.Config;
+}
+
+const configs: IKnexConfig = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : '1234',
+      database : 'lendsqrDB'
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
+export default configs;
